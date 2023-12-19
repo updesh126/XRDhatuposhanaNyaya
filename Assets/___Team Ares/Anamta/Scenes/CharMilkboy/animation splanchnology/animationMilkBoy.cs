6456148088e6deea34b58c95527577f208f9ecc0
Updesh Pr. Singh <updesh071@gmail.com>
2023-12-19 23:44:22 +0530

    update ui

---
+++ Assets/___Team Ares/Anamta/Scenes/CharMilkboy/animation splanchnology/animationMilkBoy.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditorInternal;
+using UnityEngine;
+
+public class animationMilkBoy : MonoBehaviour
+{
+    public Animator bottle;
+    public Animator drink;
+
+    public bool isBottleAnimationDone = false;
+
+    private float timer = 0f;
+
+    void Start()
+    {
+        // Play the default animation
+        PlayDefaultAnimationClip();
+    }
+    private void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > 6f && !isBottleAnimationDone)
+        {
+            PlayMilkDrinkingAnimation();
+            isBottleAnimationDone = true;
+        }
+    }
+
+    private void PlayMilkDrinkingAnimation()
+    {
+        drink.SetBool("Drink", true);
+    }
+
+    void PlayDefaultAnimationClip()
+    {
+        // Play bottle animation
+        bottle.SetBool("Bottle", true);
+    }
+}
